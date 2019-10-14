ad3b9e0d2693277b527179212e70c7bb9f713de5
LionelBeato <lionel.beato@gmail.com>
2019-10-13 20:31:01 -0400

    adds comments

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,16 @@
+
+
 fn main() {
-    let x:f32 = converter(75.0);
+    //input Fahrenheit value; note the decimal notation
+    let y:f32 = 75.0;
+    let x:f32 = converter(y);
     println!("{}", x);
 }
  
  //function that converts from fahrenheit to celcius
-
+ //note how the number type is declared and how the return type is annotated
 fn converter(temp:f32) -> f32 {
     let c = (temp-32.0)*(5.0/9.0);
+    //for functions that have explicit return values, simply stating the value below without a semicolon resolves
     c
 }
